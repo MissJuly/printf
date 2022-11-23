4fb9d0012d7ebc4bca744a465a0da231e1eb2137
MissJuly <joygitau98@gmail.com>
2022-11-24 00:44:07 +0300

    Change main.h

--- main.h
+++ main.h
@@ -1,76 +1,39 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
+ * struct format - struct to choose the right function depending
+ * on the format specifier
+ * @sp: format specifier
+ * @f: pointer to the corresponding printing function
  */
-typedef struct flags
+typedef struct format
 {
-    int plus;
-    int space;
-    int hash;
-} flags_t;
+    char sp;
+    int (*f)(va_list args);
+} fmt;
 
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-    char c;
-    int (*f)(va_list ap, flags_t *f);
-} ph;
-
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-
-/* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* _printf */
 int _printf(const char *format, ...);
-
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
-
-/* get_flag */
-int get_flag(char s, flags_t *f);
-
-/* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-
-/* write_funcs */
-int _putchar(char c);
 int _puts(char *str);
+int _putchar(char c);
+int (*get_print(char s))(va_list);
+int print_percent(va_list args);
+int print_string(va_list args);
+int print_char(va_list args);
+int print_int(va_list args);
+void print_number(int n);
+int count_digit(int x);
+int print_binary(va_list args);
+char *DectoBase(unsigned long int n, int base, int Lwr);
+int print_octal(va_list args);
+int print_hexUpr(va_list args);
+int print_hexLwr(va_list args);
+int print_unsint(va_list args);
+int print_bigS(va_list args);
+int print_address(va_list args);
 
-/* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
-/* print_percent */
-int print_percent(va_list l, flags_t *f);
-
-#endif /* MAIN_H */
+#endif
