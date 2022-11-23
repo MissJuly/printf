856dbfff7ed1be7b7fa6a9a98855bb521f6bc480
Barissa-Imran <imranbarissa@gmail.com>
2022-11-23 21:41:02 +0300

    Add _putchar and _puts in writes_funcs.c
    add _printf.c
    
    add get_flag.c and get_print.c to work in _printf

--- _printf.c
+++ _printf.c
@@ -2,37 +2,47 @@
 
 /**
  * _printf - produces output according to a format
- * @format: format string containing characters and specifiers
- * Return: total number of characters printed
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	const char *times;
-	int (*pfunc)(va_list);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	int count = 0;
+	register int count = 0;
 
-	va_start(args, format);
-
+	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-
-	for (times = format; *times; times++)
+	for (p = format; *p; p++)
 	{
-		if (*times == '%')
+		if (*p == '%')
 		{
-			times++;
-			pfunc = get_print(*times);
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
 			count += (pfunc)
-				? pfunc(args)
-				: _printf("%%%c", *times);
+						 ? pfunc(arguments, &flags)
+						 : _printf("%%%c", *p);
 		}
 		else
-			count += _putchar(*times);
+			count += _putchar(*p);
 	}
-	va_end(args);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
