dab6c3f075777c194a2051f01a3c44c60a5bb0f7
Barissa-Imran <imranbarissa@gmail.com>
2022-11-24 00:54:25 +0300

    Trial task 0

--- 0-print_alphnum.c
+++ 0-print_alphnum.c
@@ -1,51 +1,79 @@
 #include "main.h"
 
 /**
- * print_char - print a character
- * @l: va_list arguments from _printf
- * @f: pointer to the correct function
- * Return: number of char printed
+ * print_char - prints a character
+ * @args: character to print
+ * Return: 1
  */
 
-int print_char(va_list l, flags_t *f)
+int print_char(va_list args)
 {
-	(void)f;
-	_putchar(va_arg(l, int));
-
+	_putchar(va_arg(args, int));
 	return (1);
 }
 
 /**
- * print_string - loops through a string and prints
- * every character
- * @l: va_list arguments from _printf
- * @f: pointer to the correct function
- * Return: number of char printed
+ * print_string - prints a string
+ * @args: string to print
+ * Return: number of characters printed
  */
 
-int print_string(va_list l, flags_t *f)
+int print_string(va_list args)
 {
-	char *s = va_arg(l, char *);
-	(void)f;
+	int i;
+	char *str;
 
-	if (!s)
-	s = "(null)";
-
-	return (_puts(s));
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (i);
 }
 
 /**
- * print_percent - prints percent
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags in
- * which we turn the flags on
- * Return: number of char printed
+ * print_percent - prints a percent sign
+ * @args: character to print
+ * Return: 1
  */
-
-int print_percent(va_list l, flags_t *f)
+int print_percent(__attribute__((unused)) va_list args)
 {
-	(void)l;
-	(void)f;
+	_putchar('%');
+	return (1);
+}
 
-	return (_putchar('%'));
+/**
+ * print_bigS - non-printable chars are printed like this:
+ * \x folled by their hexadecimal value
+ * @args: string to print
+ * Return: no. of characters printed
+ */
+int print_bigS(va_list args)
+{
+	int i, count;
+	char *str;
+	char *c;
+
+	count = 0;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (_puts("(null)"));
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
+		{
+			_puts("\\x");
+			count += 2;
+			c = DectoBase(str[i], 16, 0);
+			if (!c[1])
+				count += _putchar('0');
+			count += _puts(c);
+		}
+		else
+			count += _putchar(str[i]);
+	}
+	return (count);
 }
--- write_funcs.c
+++ write_funcs.c
@@ -1,40 +1,24 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
+ * _putchar - prints a character
+ * @c: character to print
+ * Return: formatted output
  */
 int _putchar(char c)
 {
-    static char buf[1024];
-    static int i;
-
-    if (c == -1 || i >= 1024)
-    {
-        write(1, &buf, i);
-        i = 0;
-    }
-    if (c != -1)
-    {
-        buf[i] = c;
-        i++;
-    }
-    return (1);
+    return (write(1, &c, 1));
 }
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
+ * _puts - prints a string
+ * @str: string to print
+ * Return: no. of characters printed
  */
+
 int _puts(char *str)
 {
-    register int i;
+    int i;
 
     for (i = 0; str[i] != '\0'; i++)
         _putchar(str[i]);
