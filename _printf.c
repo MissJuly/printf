803324d68458c1ba9126b2634b83bf32a08702ad
MissJuly <joygitau98@gmail.com>
2022-11-24 11:04:33 +0300

    Re-do _printf

--- _printf.c
+++ _printf.c
@@ -1,38 +1,49 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing characters and specifiers
- * Return: total number of characters printed
+ * _printf - replication of some of the features from C function printf()
+ * @format: character string of directives, flags, modifiers, & specifiers
+ * Description: This function uses the variable arguments functionality and is
+ * supposed to resemble printf().  Please review the README for more
+ * information on how it works.
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	const char *trav;
-	int (*pfunc)(va_list);
+	va_list args_list;
+	inventory_t *inv;
+	void (*temp_func)(inventory_t *);
 
-	int count = 0;
-
-	va_start(args, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
+	if (!format)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
+	va_start(args_list, format);
+	inv = build_inventory(&args_list, format);
 
-	for (trav = format; *trav; trav++)
+	while (inv && format[inv->i] && !inv->error)
 	{
-		if (*trav == '%')
-		{
-			trav++;
-			pfunc = get_print(*trav);
-			count += (pfunc)
-						 ? pfunc(args)
-						 : _printf("%%%c", *trav);
-		}
+		inv->c0 = format[inv->i];
+		if (inv->c0 != '%')
+			write_buffer(inv);
 		else
-			count += _putchar(*trav);
+		{
+			parse_specifiers(inv);
+			temp_func = match_specifier(inv);
+			if (temp_func)
+				temp_func(inv);
+			else if (inv->c1)
+			{
+				if (inv->flag)
+					inv->flag = 0;
+				write_buffer(inv);
+			}
+			else
+			{
+				if (inv->space)
+					inv->buffer[--(inv->buf_index)] = '\0';
+				inv->error = 1;
+			}
+		}
+		inv->i++;
 	}
-	va_end(args);
-	return (count);
+	return (end_func(inv));
 }
--- main.h
+++ main.h
@@ -1,39 +1,114 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* begin standard C header files */
+#include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+/* macros */
+
+#define BUFSIZE 1024
+#define TRUE (1 == 1)
+#define FALSE !TRUE
+#define LOWHEX 0
+#define UPHEX 1
+
+/* structs */
+/**
+ * struct inventory_s - inventory of common variables needed
+ * @fmt: the input format string
+ * @i: index to traverse the format string
+ * @args: the variadic arguments list of input arguments
+ * @buffer: buffer to be written to before writing to stdout
+ * @buf_index: index to traverse the buffer, also total chars written
+ * @flag: notifies if there was a modifier flag
+ * @space: notifies if space was printed
+ * @c0: character to be written to buffer
+ * @c1: character checking after % character
+ * @c2: character to check 2 spaces after % symbol
+ * @c3: unused for now, but may become a third specifier
+ * @error: indicates error or not (0 no error, 1 error)
+ */
+typedef struct inventory_s
+{
+	const char *fmt;
+	int i;
+	va_list *args;
+	char *buffer;
+	int buf_index;
+	int flag;
+	int space;
+	char c0;
+	char c1;
+	char c2;
+	char c3;
+	int error;
+} inventory_t;
 
 /**
- * struct format - struct to choose the right function depending
- * on the format specifier
- * @sp: format specifier
- * @f: pointer to the corresponding printing function
+ * struct matches_s - printf specifiers and paired function
+ * @ch: the specifier
+ * @func: pointer to the conversion specifier function
  */
-typedef struct format
+typedef struct matches_s
 {
-    char sp;
-    int (*f)(va_list args);
-} fmt;
+	char ch;
+	void (*func)(inventory_t *inv);
+} matches_t;
 
+/* initializing and ending functions */
 int _printf(const char *format, ...);
-int _puts(char *str);
-int _putchar(char c);
-int (*get_print(char s))(va_list);
-int print_percent(va_list args);
-int print_string(va_list args);
-int print_char(va_list args);
-int print_int(va_list args);
-void print_number(int n);
-int count_digit(int x);
-int print_binary(va_list args);
-char *DectoBase(unsigned long int n, int base, int Lwr);
-int print_octal(va_list args);
-int print_hexUpr(va_list args);
-int print_hexLwr(va_list args);
-int print_unsint(va_list args);
-int print_bigS(va_list args);
-int print_address(va_list args);
+inventory_t *build_inventory(va_list *args_list, const char *format);
+int end_func(inventory_t *arg_inv);
 
-#endif
+/* custom memory allocation and buffer */
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void write_buffer(inventory_t *inv);
+void puts_buffer(inventory_t *inv, char *str);
+
+/* string functions */
+void rev_string(char *s);
+int _strlen(char *s);
+int _strlenconst(const char *s);
+int _putchar(char c);
+void puts_mod(char *str, unsigned int l);
+
+/* parse and match functionality */
+void (*match_specifier(inventory_t *inv))(inventory_t *inv);
+void parse_specifiers(inventory_t *inv);
+
+/* hexadecimal */
+void print_hex(inventory_t *inv, unsigned long int n, int hexcase, int size);
+void p_longlowhex(inventory_t *inv);
+void p_longuphex(inventory_t *inv);
+void p_lowhex(inventory_t *inv);
+void p_uphex(inventory_t *inv);
+
+/* integers */
+void print_integers(inventory_t *inv, long int n);
+void p_int(inventory_t *inv);
+void p_longint(inventory_t *inv);
+void print_unsign(inventory_t *inv, unsigned long int n);
+void p_uint(inventory_t *inv);
+void p_ulongint(inventory_t *inv);
+
+/* octals */
+void print_oct(inventory_t *inv, unsigned long int n, int size);
+void p_oct(inventory_t *inv);
+void p_longoct(inventory_t *inv);
+
+/* handles specifier functions */
+void p_char(inventory_t *inv);
+void p_string(inventory_t *inv);
+void p_string_hex(inventory_t *inv);
+void p_pointer(inventory_t *inv);
+void p_rev_string(inventory_t *inv);
+void p_rot13(inventory_t *inv);
+void p_percent(inventory_t *inv);
+void p_binary(inventory_t *inv);
+
+#endif /* MAIN_H */
